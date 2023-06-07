021480fdd2b0cb15985d96e0e041e89e67fec746
Natalia <natalia.alvarez03@uceva.edu.co>
2023-06-06 20:20:36 -0500

    Añade la conexion a la base de datos e impor sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO cursos (nombre) VALUES ('Primer semestre');
+INSERT INTO cursos (nombre) VALUES ('Segundo semestre');
+INSERT INTO cursos (nombre) VALUES ('Tercer semestre');
+INSERT INTO cursos (nombre) VALUES ('Cuarto semestre');
+INSERT INTO cursos (nombre) VALUES ('Quinto semestre');
+INSERT INTO cursos (nombre) VALUES ('Sexto semestre');
